d278be6ab9c77f46bb6b2fe16df4d8be7a540e3f
Jose Miguel <malikjmiguel0@gmail.com>
2023-10-26 15:54:14 -0300

    adiciona e coda No.h

---
+++ No.h
@@ -0,0 +1,55 @@
+#ifndef NO_H_INCLUDED
+#define NO_H_INCLUDED
+
+#include <iostream>
+#include <stdlib.h>
+#include "Aresta.h"
+
+using namespace std;
+
+class No{  ///Falta DESTRUTOR 
+    private:
+        int idNo;
+        float pesoNo;
+        int grauEntrada;
+        int grauSaida;
+        bool visitado;
+        No* proxNo;
+        Aresta* primeiraAresta;
+        Aresta* ultimaAresta;
+
+    public:
+        //GETTERS
+        int getIdNo();
+        float getPesoNo();
+        int getGrauEntrada();
+        int getGrauSaida();
+        bool foiVisitado();
+        No* getProxNo();
+        Aresta* getPrimeiraAresta();
+        Aresta* getUltimaAresta();
+        Aresta* getAresta(int idNoDestino);
+
+        //SETTERS
+        void setProxNo(No* no);
+        void setPesoNo(float peso);
+        void setVisita(bool visitadoOuNao);
+
+        //OUTROS METODOS
+        bool procuraAresta(int idNoDestino);
+        void insereAresta(int idNoDestino, float peso);
+        void removeTodasArestas();
+        //int removeAresta(int id, bool directed, No* noDestino); //não entendi os parâmetros
+        void incrementaGrauSaida();
+        void decrementaGrauSaida();
+        void incrementaGrauEntrada();
+        void decrementaGrauEntrada();
+        Aresta* temArestaEntre(int idNoDestino);
+        //bool verificaAresta(int idNoDestino); //pensei que fosse igual "procuraAresta"
+
+        //METODOS AUXILIARES
+        
+
+};
+
+#endif //NO_H_INCLUDED
